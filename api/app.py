16752b6755e69b7c51c3173fc747e413b8e2d931
Abhronil Dutta <177771087+Abhronil-Dutta@users.noreply.github.com>
2026-01-10 20:58:20 -0700

    Added Database connection and completed api

--- api/app.py
+++ api/app.py
@@ -1,25 +1,59 @@
-from flask import Flask, jsonify, request
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import text
+from .db import SessionLocal
 
-app = Flask(__name__)
+from pydantic import BaseModel
+from sqlalchemy import text
+import json
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    return jsonify({"status": "up", "message": "Service is running"}), 200
+app = FastAPI()
 
-@app.route('/api/resource', methods=['GET'])
-def get_resource():
-    return jsonify({"data": "sample resource"}), 200
+class JobCreate(BaseModel):
+    job_type: str
+    payload: dict
+    max_attempts: int = 5
 
-@app.route('/api/resource', methods=['POST'])
-def create_resource():
-    data = request.get_json()
-    if not data:
-        return jsonify({"error": "Invalid payload"}), 400
-    return jsonify({"received": data}), 201
 
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.get("/db_check")
+def db_check():
+    db = SessionLocal()
+
+    try:
+        result = db.execute(text("SELECT 1")).scalar()
+        return {"db":"connected",
+                "result": result
+                }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"failed to get database {e}")
+    finally:
+        db.close()
+
+@app.post("/jobs")
+def create_jon(job: JobCreate):
+    db = SessionLocal()
+    try:
+        query = text("""
+            INSERT INTO jobs(job_type, payload, max_attempts)
+            VALUES (:job_type, :payload, :max_attemps)
+            RETURNING id, status
+
+        """)
+        result = db.execute(query, {
+            "job_type": job.job_type,
+            "payload": json.dumps(job.payload),
+            "max_attemps": job.max_attempts,
+        },
+        ).fetchone()
+        db.commit()
+
+        return {
+            "job_id": str(result.id),
+            "status": result.status
+        }
+    finally:
+        db.close()
+
